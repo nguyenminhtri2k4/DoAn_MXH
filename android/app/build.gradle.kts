243ff0e408ae79bd6be975a0346c179fc2bd8ffa
Hà Minh Tiến <minhtien250704@gmail.com>
2025-11-18 14:50:55 +0700

    Fix lỗi thời gian

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mangxahoi"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
